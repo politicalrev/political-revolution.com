00b0b1bd285f0d727484f21edc30c35901f3262b
Amanda Robertson <amanda@innovativedmm.com>
2017-01-09 17:57:11 -0500

    Add meta tags
    Add twitter meta tags. Removed the google font call from head.

--- web/app/themes/thepoliticalsage/templates/head.php
+++ web/app/themes/thepoliticalsage/templates/head.php
@@ -2,8 +2,18 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta itemprop="Organization" CONTENT="The Political Revolution is a collective of volunteers that grew out of the Bernie Sanders campaign. We’re an organization focused on digital campaigning, and connecting progressives across the nation. Our goal is to support progressive candidates and ballot initiatives on a state-by-state message, and to amplify the message of other like-minded organizations and campaigns.">
   <meta name="google-site-verification" content="GeEavsoPBp10LOO68IiuDH16WRABk-3TD1pSs5-dVXU" />
-  <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500" rel="stylesheet">
+  <title>The Political Revolution | National Grassroots Volunteer Hub</title>
+  <!-- Twitter Card -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@ourpoliticalrev">
+  <meta name="twitter:creator" content="@ourpoliticalrev">
+  <meta name="twitter:title" content="Change can only take place when people get involved in the political process. Now is the time to join the revolution!">
+  <meta name="twitter:description" content="National grassroots hub for progressive candidates, volunteers, and anyone intersted in changed the face of our government.">
+  <meta name="twitter:image" content="https://political-revolution.com/wp-content/themes/thepoliticalsage/assets/images/logo-web.png">
+  <meta name="robots" content="noindex,nofollow"> 
+  
   <?php wp_head(); ?>
   <?php if (is_page_template('templates/template-petition.php')) : ?>
   
