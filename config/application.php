ffe58112c74366861a371494db80b38833c2ee46
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-10 00:08:05 -0600

    finish w3tc installation

--- config/application.php
+++ config/application.php
@@ -74,6 +74,8 @@ define('NONCE_SALT', env('NONCE_SALT'));
 define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 define('DISALLOW_FILE_MODS', true);
+/** Enable W3 Total Cache */
+define('WP_CACHE', true); // Added by W3 Total Cache
 
 /**
  * Bootstrap WordPress
