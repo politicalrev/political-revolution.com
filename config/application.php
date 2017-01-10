f42b39396a25e0fa41a9bc4a24fedeef5ccfa2ea
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-09 21:23:20 -0600

    Disallow server-side plugin and theme installation

--- config/application.php
+++ config/application.php
@@ -73,7 +73,7 @@ define('NONCE_SALT', env('NONCE_SALT'));
  */
 define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
-define('DISALLOW_FILE_EDIT', true);
+define('DISALLOW_FILE_MODS', true);
 
 /**
  * Bootstrap WordPress
