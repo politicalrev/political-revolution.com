005da2f1d2167e82f8d23a56b9c4b7c7fd5cd151
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-11 15:18:04 -0600

    Re-add Work Sans link

--- web/app/themes/thepoliticalsage/templates/head.php
+++ web/app/themes/thepoliticalsage/templates/head.php
@@ -5,6 +5,7 @@
   <meta itemprop="Organization" CONTENT="The Political Revolution is a collective of volunteers that grew out of the Bernie Sanders campaign. We’re an organization focused on digital campaigning, and connecting progressives across the nation. Our goal is to support progressive candidates and ballot initiatives on a state-by-state message, and to amplify the message of other like-minded organizations and campaigns.">
   <meta name="google-site-verification" content="GeEavsoPBp10LOO68IiuDH16WRABk-3TD1pSs5-dVXU" />
   <title>The Political Revolution | National Grassroots Volunteer Hub</title>
+  
   <!-- Twitter Card -->
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:site" content="@ourpoliticalrev">
@@ -12,7 +13,10 @@
   <meta name="twitter:title" content="Change can only take place when people get involved in the political process. Now is the time to join the revolution!">
   <meta name="twitter:description" content="National grassroots hub for progressive candidates, volunteers, and anyone intersted in changed the face of our government.">
   <meta name="twitter:image" content="https://political-revolution.com/wp-content/uploads/2017/01/TPR-twittercard.jpg">
-  <meta name="robots" content="noindex,nofollow"> 
+  <meta name="robots" content="noindex,nofollow">
+
+  <!-- Web Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500" rel="stylesheet"> 
   
   <?php wp_head(); ?>
   <?php if (is_page_template('templates/template-petition.php')) : ?>
