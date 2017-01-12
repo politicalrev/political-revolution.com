502378c741481bc4b78478d4508d2d958983f0da
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-11 22:28:12 -0600

    Add Facebook SDK

--- web/app/themes/thepoliticalsage/base.php
+++ web/app/themes/thepoliticalsage/base.php
@@ -9,16 +9,28 @@ use Roots\Sage\Wrapper;
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+ 
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
+
+    <!--Import Facebook SDK-->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=484292181660884";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
     <?php
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-          <?php include Wrapper\template_path(); ?>
+        <?php include Wrapper\template_path(); ?>
         <?php if (Setup\display_sidebar()) : ?>
           <aside class="sidebar">
             <?php include Wrapper\sidebar_path(); ?>
