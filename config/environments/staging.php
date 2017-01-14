adf457d3d4314b7e9e4e4b819dea871d5b37c92c
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-14 02:46:14 -0600

    Fix config syntax error

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -2,7 +2,7 @@
 /** Staging */
 // ini_set('display_errors', 0);
 define('WP_DEBUG', true);
-define('WP_DEBUG_DISPLAY', true;
+define('WP_DEBUG_DISPLAY', true);
 define('SCRIPT_DEBUG', true);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
