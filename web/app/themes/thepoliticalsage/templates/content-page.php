fd881cd4c614ed3867690306b99ebde4b92f7fce
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-15 16:52:52 -0600

    Remove default styling from pages
    Also convert page styles to scss for consistency

--- web/app/themes/thepoliticalsage/templates/content-page.php
+++ web/app/themes/thepoliticalsage/templates/content-page.php
@@ -1,15 +1,13 @@
 <section class="page responsive-text">
-    <section class="text-wrapper">
-        <div class="letterhead">
-            <i class="fa fa-star star"></i> 
-            <i class="fa fa-star star"></i> 
-            <i class="fa fa-star star"></i> 
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/button.svg" alt="TPR Logo"> 
-            <i class="fa fa-star star"></i> 
-            <i class="fa fa-star star"></i> 
-            <i class="fa fa-star star"></i>
-        </div>
-        <?php the_content(); ?>
-    </section>
+    <!--<div class="letterhead">
+        <i class="fa fa-star star"></i> 
+        <i class="fa fa-star star"></i> 
+        <i class="fa fa-star star"></i> 
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/button.svg" alt="TPR Logo"> 
+        <i class="fa fa-star star"></i> 
+        <i class="fa fa-star star"></i> 
+        <i class="fa fa-star star"></i>
+    </div>-->
+    <?php the_content(); ?>
 </section>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
