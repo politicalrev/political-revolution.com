c5ac44020ea990dc223a437720f485562404a66f
Nathan Bakirci <nathan@virtru.com>
2017-01-20 22:40:39 -0500

    Add social link hover styling and markup

--- web/app/themes/thepoliticalsage/templates/footer.php
+++ web/app/themes/thepoliticalsage/templates/footer.php
@@ -13,15 +13,45 @@
     <footer id="footer">
             <div>
                 <ul class="icons">
-                    <li><a href="https://twitter.com/ourpoliticalrev" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
-                    <li><a href="https://www.facebook.com/ThePoliticalRev/" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+                    <li>
+                        <a href="https://twitter.com/ourpoliticalrev" class="icon fa-twitter" target="_blank">
+                            <span class="label">Twitter</span>
+                            <div class="icon-detail">@OurPoliticalRev</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/ThePoliticalRev/" class="icon fa-facebook" target="_blank">
+                            <span class="label">Facebook</span>
+                            <div class="icon-detail">Facebook</div>
+                        </a>
+                    </li>
                     <!-- {% if site.gitlab_url %}
                     <li><a href="{{ site.gitlab_url }}" class="icon fa-gitlab" target="_blank"><span class="label">GitLab</span></a></li>
                     {% endif %} -->
-                    <li><a href="https://github.com/politicalrev" class="icon fa-github" target="_blank"><span class="label">GitHub</span></a></li>
-                    <li><a href="https://medium.com/voices-of-the-political-revolution" class="icon fa-medium" target="_blank"><span class="label">Medium</span></a></li>
-                    <li><a href="https://polrevvols.herokuapp.com/" class="icon fa-slack" target="_blank"><span class="label">Slack</span></a></li>
-                    <li><a href="https://www.reddit.com/r/Political_Revolution/" class="icon fa-reddit-alien" target="_blank"><span class="label">LinkedIn</span></a></li>
+                    <li>
+                        <a href="https://github.com/politicalrev" class="icon fa-github" target="_blank">
+                            <span class="label">GitHub</span>
+                            <div class="icon-detail">Github</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://medium.com/voices-of-the-political-revolution" class="icon fa-medium" target="_blank">
+                            <span class="label">Medium</span>
+                            <div class="icon-detail">Voices</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://polrevvols.herokuapp.com/" class="icon fa-slack" target="_blank">
+                            <span class="label">Slack</span>
+                            <div class="icon-detail">Chat</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.reddit.com/r/Political_Revolution/" class="icon fa-reddit-alien" target="_blank">
+                            <span class="label">LinkedIn</span>
+                            <div class="icon-detail">/r/Political_Revolution</div>
+                        </a>
+                    </li>
                 </ul>
             </div>
             <div>
