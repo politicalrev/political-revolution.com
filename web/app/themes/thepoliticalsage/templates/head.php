d68f81f0b9b8e69a222a055998325f8b918d7c79
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-26 11:01:36 -0600

    Remove Google site Verification from head as this can be managed from Yoast

--- web/app/themes/thepoliticalsage/templates/head.php
+++ web/app/themes/thepoliticalsage/templates/head.php
@@ -2,8 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Custom meta tags not handled by Yoast -->
   <meta itemprop="Organization" CONTENT="The Political Revolution is a collective of volunteers that grew out of the Bernie Sanders campaign. We’re an organization focused on digital campaigning, and connecting progressives across the nation. Our goal is to support progressive candidates and ballot initiatives on a state-by-state message, and to amplify the message of other like-minded organizations and campaigns.">
-  <meta name="google-site-verification" content="GeEavsoPBp10LOO68IiuDH16WRABk-3TD1pSs5-dVXU" />
   
   <!-- Web Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500" rel="stylesheet"> 
