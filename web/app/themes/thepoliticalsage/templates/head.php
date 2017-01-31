709fb36fb5c3af1c0270de0d481c3fe1112304cc
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-30 21:31:21 -0600

    Add basic app manifest configuration

--- web/app/themes/thepoliticalsage/templates/head.php
+++ web/app/themes/thepoliticalsage/templates/head.php
@@ -1,9 +1,19 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- Custom meta tags not handled by Yoast -->
   <meta itemprop="Organization" CONTENT="The Political Revolution is a collective of volunteers that grew out of the Bernie Sanders campaign. We’re an organization focused on digital campaigning, and connecting progressives across the nation. Our goal is to support progressive candidates and ballot initiatives on a state-by-state message, and to amplify the message of other like-minded organizations and campaigns.">
+  <link rel="manifest" href="manifest.json">
+
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="application-name" content="TPR">
+  <meta name="apple-mobile-web-app-title" content="TPR">
+  <meta name="theme-color" content="#04243E">
+  <meta name="msapplication-navbutton-color" content="#04243E">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="msapplication-starturl" content="/">
   
   <!-- Web Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500" rel="stylesheet"> 
