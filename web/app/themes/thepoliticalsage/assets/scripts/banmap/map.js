401c69471e43d2cb6f2393c458a7157a56b640f4
chris <karp505@gmail.com>
2017-02-05 14:44:01 -0500

    code climate cleanup

--- web/app/themes/thepoliticalsage/assets/scripts/banmap/map.js
+++ web/app/themes/thepoliticalsage/assets/scripts/banmap/map.js
@@ -2,27 +2,15 @@ var d3 = require('./d3.min');
 var senators = require('./senators');
 var uStates = require('./uStates');
 
+function senatorHtml(d){
+	var senBody = "<div class='col-xs-6' id='tooltip-body-col'><div class='row'><h6>"+(d["First Name"] + " " + d["Last Name"])+"</h6></div><div class='row'><p>"+(d["Stance"])+"</p></div><div class='row'><p>";
+	senBody += d["Statement"];
+	senBody += "</p></div><div class='row'><a href = '"+ (d["Statement Link"]) + "'><p>"+(d["Statement Link"])+"</a></p></div><div class='row'><p>"+(d["Local Office Number"])+"</p></div></div>";
+	return senBody;
+}
+
 function tooltipHtml(n, d){
-	var elem = "<h4>"+n+"</h4><div class='row'>"+
-		"<div class='col-xs-6' id='tooltip-body-col'><div class='row'><h6>"+(d[0]["First Name"] + " " + d[0]["Last Name"])+"</h6></div><div class='row'><p>"+(d[0]["Stance"])+"</p></div><div class='row'><p>";
-
-	var addQuotes = d[0]["Statement"].indexOf('"') == -1;
-	//if (addQuotes) elem += '"';
-	elem += d[0]["Statement"];
-	//if (addQuotes) elem += '"';
-
-	elem += "</p></div><div class='row'><a href = '"+ (d[0]["Statement Link"]) + "'><p>"+(d[0]["Statement Link"])+"</a></p></div><div class='row'><p>"+(d[0]["Local Office Number"])+"</p></div></div>"+
-		"<div class='col-xs-6' id='tooltip-body-col'><div class='row'><h6>"+(d[1]["First Name"] + " " + d[1]["Last Name"])+"</h6></div><div class='row'><p>"+(d[1]["Stance"])+"</p></div><div class='row'><p>"
-
-	addQuotes = d[1]["Statement"].indexOf('"') == -1;
-	//if (addQuotes) elem += '"';
-	elem += d[1]["Statement"];
-	//if (addQuotes) elem += '"';
-
-	elem += "</p></div><div class='row'><a href = '"+ (d[1]["Statement Link"]) + "'><p>"+(d[1]["Statement Link"])+"</a></p></div><div class='row'><p>"+(d[1]["Local Office Number"])+"</p></div></div>"+
-		"</div></div>";
-
-	return elem;
+	return "<h4>"+n+"</h4><div class='row'>" + senatorHtml(d[0]) + senatorHtml(d[1]) +"</div></div>";
 }
 
 d3.select("div#map-container")
