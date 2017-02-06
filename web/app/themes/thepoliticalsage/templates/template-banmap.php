0f47bcc10823b2515a68886042cabbe4a347f71e
Shaderboy <karp505@gmail.com>
2017-02-06 16:26:12 -0500

    fixed image paths, along with some style cleanup (#42)
    * added interactive muslim ban senate map
    
    * code climate cleanup
    
    * redid indentations to use spaces according to style rules
    
    * fixed image paths
    
    * reworked image paths again to use wp pathing

---
+++ web/app/themes/thepoliticalsage/templates/template-banmap.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Template Name: Petition
+ */
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Accountability</title>
+  </head>
+  <body><img id="background" src="../assets/images/banmap//background.png">
+    <div class="container-fluid">
+      <div class="row">
+        <div id="header-col" class="col-xs-12">
+          <div id="header"> 
+            <h1>The Muslim Ban According to The Senate</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-2 col-lg-0">
+          <div id="legend">
+            <div id="big-screen-legend">
+              <div class="row legend-row"><img id="oppose-icon" src="../assets/images/banmap/teal_icon.png">
+                <p>Fully Opposed</p>
+              </div>
+              <div class="row legend-row"><img id="half-icon" src="../assets/images/banmap/blue_icon.png">
+                <p>Split</p>
+              </div>
+              <div class="row legend-row"><img id="support-icon" src="../assets/images/banmap/black_icon.png">
+                <p>Fully Supportive</p>
+              </div>
+            </div>
+            <div id="small-screen-legend" class="legend-row">
+              <div class="row">
+                <div class="col-xs-4">
+                  <div class="row"><img id="oppose-icon" src="../assets/images/banmap/teal_icon.png" class="legend-icon"></div>
+                  <div class="row">
+                    <p>Fully Opposed</p>
+                  </div>
+                </div>
+                <div class="col-xs-4">
+                  <div class="row"><img id="half-icon" src="../assets/images/banmap/blue_icon.png" class="legend-icon"></div>
+                  <div class="row">
+                    <p>Split</p>
+                  </div>
+                </div>
+                <div class="col-xs-4">
+                  <div class="row"><img id="support-icon" src="../assets/images/banmap/black_icon.png" class="legend-icon"></div>
+                  <div class="row">
+                    <p>Fully Supportive</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-md-8 col-lg-8">
+          <div id="tooltip">
+            <div class="container"><img id="close-tooltip" src="../assets/images/banmap/close.png" style="cursor: pointer"></div>
+            <div id="body"></div>
+          </div>
+          <div id="map-container"></div>
+          <!--svg#statesvg(width="960" height="600")-->
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
