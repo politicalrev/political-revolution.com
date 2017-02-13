cf78e9fe8694ed01fc573534be6857f7c4c19f07
bpan <bpan@guidewire.com>
2017-02-12 23:44:46 -0800

    Added footer disclaimer text ("Paid for by...")
    * Removed the "nav" list because they weren't actually clickable links
    * Added semantic classes to divs
    * Fixed a typo in _button.scss (No visible change)

--- web/app/themes/thepoliticalsage/templates/footer.php
+++ web/app/themes/thepoliticalsage/templates/footer.php
@@ -1,67 +1,74 @@
-    </main>
-    <!-- Main -->
+</main>
+<!-- Main -->
 
 
-    <!--<footer class="content-info">
+<!--<footer class="content-info">
       <div class="container">
         <?php
-        //dynamic_sidebar('sidebar-footer');
-        ?>
+//dynamic_sidebar('sidebar-footer');
+?>
       </div>
     </footer>-->
 
-    <footer id="footer">
-            <div>
-                <ul class="icons">
-                    <li>
-                        <a href="https://twitter.com/ourpoliticalrev" class="icon fa-twitter" target="_blank">
-                            <span class="label">Twitter</span>
-                            <div class="icon-detail">@OurPoliticalRev</div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.facebook.com/ThePoliticalRev/" class="icon fa-facebook" target="_blank">
-                            <span class="label">Facebook</span>
-                            <div class="icon-detail">Facebook</div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/politicalrev" class="icon fa-github" target="_blank">
-                            <span class="label">GitHub</span>
-                            <div class="icon-detail">Github</div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://medium.com/voices-of-the-political-revolution" class="icon fa-medium" target="_blank">
-                            <span class="label">Medium</span>
-                            <div class="icon-detail">Voices</div>
-                        </a>
-                    </li>
-                    <!--<li>
-                        <a href="https://polrevvols.herokuapp.com/" class="icon fa-slack" target="_blank">
-                            <span class="label">Slack</span>
-                            <div class="icon-detail">Chat</div>
-                        </a>
-                    </li>-->
-                    <li>
-                        <a href="https://www.reddit.com/r/Political_Revolution/" class="icon fa-reddit-alien" target="_blank">
-                            <span class="label">LinkedIn</span>
-                            <div class="icon-detail">/r/Political_Revolution</div>
-                        </a>
-                    </li>
-                </ul>
+<footer id="footer">
+    <div class="social">
+        <ul class="icons">
+            <li>
+                <a href="https://twitter.com/ourpoliticalrev" class="icon fa-twitter" target="_blank">
+                    <span class="label">Twitter</span>
+                    <div class="icon-detail">@OurPoliticalRev</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/ThePoliticalRev/" class="icon fa-facebook" target="_blank">
+                    <span class="label">Facebook</span>
+                    <div class="icon-detail">Facebook</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/politicalrev" class="icon fa-github" target="_blank">
+                    <span class="label">GitHub</span>
+                    <div class="icon-detail">Github</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://medium.com/voices-of-the-political-revolution" class="icon fa-medium" target="_blank">
+                    <span class="label">Medium</span>
+                    <div class="icon-detail">Voices</div>
+                </a>
+            </li>
+            <!--<li>
+                <a href="https://polrevvols.herokuapp.com/" class="icon fa-slack" target="_blank">
+                    <span class="label">Slack</span>
+                    <div class="icon-detail">Chat</div>
+                </a>
+            </li>-->
+            <li>
+                <a href="https://www.reddit.com/r/Political_Revolution/" class="icon fa-reddit-alien" target="_blank">
+                    <span class="label">LinkedIn</span>
+                    <div class="icon-detail">/r/Political_Revolution</div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="legal">
+        <span class="footer-left">&copy; <?= bloginfo('title'); ?> 2017</span>
+        <img alt="Political Revolution Bird Logo"
+             src="<?= get_stylesheet_directory_uri(); ?>/assets/images/button.svg"/>
+        <!--                <ul class="footernav">-->
+        <!--                    <li>Contact Us</li>-->
+        <!--                    <li>Search</li>-->
+        <!--                    <li>By-laws</li>-->
+        <!--                    <li>Privacy Policy</li>-->
+        <!--                </ul>-->
+        <span class="footer-right">
+            <div class="disclaimer">
+                Paid for by The Political Revolution (<a href="<?= home_url(); ?>">political-revolution.com</a>). Not
+                authorized by any candidate or candidate's committee.
             </div>
-            <div>
-                <span class="copyright">&copy; <?= bloginfo('title'); ?> 2017</span>
-                <img alt="Political Revolution Bird Logo" src="<?= get_stylesheet_directory_uri(); ?>/assets/images/button.svg" />
-                <ul class="footernav">
-                    <li>Contact Us</li>
-                    <li>Search</li>
-                    <li>By-laws</li>
-                    <li>Privacy Policy</li>
-                </ul>
-            </div>
-    </footer>
+        </span>
+    </div>
+</footer>
 
 </div>
 <!-- Wrapper -->
